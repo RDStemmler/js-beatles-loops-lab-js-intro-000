bfe4a8ebe17d19df06b8a2a3dc98e52b6a07715b
Rob <rdstemmler@gmail.com>
2017-10-29 20:24:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for(var i = 0; i < musicians.length; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles
+}
